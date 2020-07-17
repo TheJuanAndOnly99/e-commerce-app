9a67d37abdb3f68e57533eca3f855d473bc176c4
Juan Estrella <juan.estrella137@gmail.com>
2020-07-17 08:50:29 +0200

    Create a get response using the express module

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log('Hello');
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('hi');
+});
+
+app.listen(3000, () => {
+  console.log('Listening');
+});
