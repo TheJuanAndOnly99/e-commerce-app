eb03c7664b91f9fc3637e39eb1b1df4308759433
Juan Estrella <juan.estrella137@gmail.com>
2020-07-19 14:12:38 +0200

    Restructure project architecture, create views folder to handle all html rendering

--- routes/admin/auth.js
+++ routes/admin/auth.js
@@ -1,19 +1,12 @@
 const express = require('express');
 const usersRepo = require('../../repositories/users');
+const signupTemplate = require('../../views/admin/auth/signup');
+const signinTemplate = require('../../views/admin/auth/signin');
 
 const router = express.Router();
 
 router.get('/signup', (req, res) => {
-  res.send(`
-    <div>      
-      <form method="POST">
-        <input name="email" placeholder="email" />
-        <input name="password" placeholder="password" />
-        <input name="passwordConfirmation" placeholder="password confirmation" />
-        <button>Sign up</button>
-      </form>
-    </div>    
-  `);
+  res.send(signupTemplate({ req }));
 });
 
 router.post('/signup', async (req, res) => {
@@ -41,15 +34,7 @@ router.get('/signout', (req, res) => {
 });
 
 router.get('/signin', (req, res) => {
-  res.send(`
-  <div>      
-    <form method="POST">
-      <input name="email" placeholder="email" />
-      <input name="password" placeholder="password" />
-      <button>Sign in</button>
-    </form>
-  </div>    
-  `);
+  res.send(signinTemplate({ req }));
 });
 
 router.post('/signin', async (req, res) => {
