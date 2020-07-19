4e185d8b751fc971d23bd969716d4d925e218a75
Juan Estrella <juan.estrella137@gmail.com>
2020-07-19 21:02:11 +0200

    Restructure validation logic

---
+++ routes/admin/validators.js
@@ -0,0 +1,16 @@
+const { check } = require('express-validator');
+const usersRepo = require('../../repositories/users');
+
+module.exports = {
+  requireEmail : check('email')
+    .trim()
+    .normalizeEmail()
+    .isEmail()
+    .withMessage('Must be a valid email')
+    .custom(async (email) => {
+      const existingUser = await usersRepo.getOneBy({ email });
+      if (existingUser) {
+        throw new Error('Email in use');
+      }
+    })
+};
