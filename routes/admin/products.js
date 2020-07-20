7909482af0a57acc217930c988a108cea16f85b6
Juan Estrella <juan.estrella137@gmail.com>
2020-07-20 15:50:30 +0200

    Import and call handleErrors function

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -2,6 +2,7 @@ const express = require('express');
 const { validationResult } = require('express-validator');
 const multer = require('multer');
 
+const { handleErrors } = require('./middleware');
 const productsRepo = require('../../repositories/products');
 const productsNewTemplate = require('../../views/admin/products/new');
 const { requireTitle, requirePrice } = require('./validators');
